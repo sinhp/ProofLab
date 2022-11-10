6b2696ba89b0b44ace575d4e3820b23f542cadcf
introproofs <sinahazratpour@gmail.com>
2022-11-09 23:55:35 -0500

    Create hw10.lean

---
+++ src/homework/hw10.lean
@@ -0,0 +1,22 @@
+/- 
+Homeowork 10  
+Sina Hazratpour
+Introduction to Proof  
+MATH 301, Johns Hopkins University, Fall 2022   
+-/
+
+
+import ..prooflab
+import lectures.lec10_surj_inj_fact
+import lectures.lec11_type_classes
+import lectures.lec13_structures_on_gaussian_int
+
+
+
+open PROOFS 
+open PROOFS.STR 
+
+
+
+
+
